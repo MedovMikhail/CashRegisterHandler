20db7d5798ff07a838304eab5e3245a9ba27b2a4
medovmisha <medovmikhail03@mail.ru>
2025-05-10 18:49:51 +0300

    add validation and remove needless field

--- src/main/java/com/example/CashRegisterHandler/dto/ExchangeValuesDTO.java
+++ src/main/java/com/example/CashRegisterHandler/dto/ExchangeValuesDTO.java
@@ -1,5 +1,6 @@
 package com.example.CashRegisterHandler.dto;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,12 @@ import java.math.BigDecimal;
 @NoArgsConstructor
 public class ExchangeValuesDTO {
 
+    @NotNull
     private BigDecimal baseCurrencyCount;
-    private BigDecimal targetCurrencyCount;
+    @NotNull
     private BigDecimal exchangeRate;
+    @NotNull
     private BigDecimal baseStoredCurrency;
+    @NotNull
     private BigDecimal targetStoredCurrency;
 }
