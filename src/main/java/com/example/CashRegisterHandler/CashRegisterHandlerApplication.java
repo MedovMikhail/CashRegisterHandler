131512e18815940ba858492c7a441e6dbaab457c
medovmisha <medovmikhail03@mail.ru>
2025-05-10 16:04:53 +0300

    initial commit

---
+++ src/main/java/com/example/CashRegisterHandler/CashRegisterHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CashRegisterHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashRegisterHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashRegisterHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CashRegisterHandler/CashRegisterHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CashRegisterHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashRegisterHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
